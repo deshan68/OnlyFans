ed52ed07c4dd35302c83dfa37837a3430f820846
deshan68 <arundeshan>
2023-06-13 19:07:32 +0530

    add dynamic profile page and style some components

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,36 +5,45 @@ import {
   View,
   Image,
   ImageBackground,
+  Pressable,
 } from "react-native";
+import { Link } from "expo-router";
 
 export default function UserCard({ user }) {
   return (
-    <ImageBackground source={{ uri: user.coverImage }} style={styles.userCards}>
-      <View style={styles.overLay} />
-      {/* image */}
-      <Image src={user.avatar} style={styles.userImage} />
+    <Link href={`/user/${user.id}`} asChild>
+      <Pressable>
+        <ImageBackground
+          source={{ uri: user.coverImage }}
+          style={styles.userCards}
+        >
+          <View style={styles.overLay} />
+          {/* image */}
+          <Image src={user.avatar} style={styles.userImage} />
 
-      {/* name and handle */}
-      <View>
-        <Text
-          style={{
-            color: "white",
-            fontSize: 22,
-            fontWeight: "500",
-            marginBottom: 5,
-          }}
-        >
-          {user.name}
-        </Text>
-        <Text
-          style={{
-            color: "white",
-          }}
-        >
-          @{user.handle}
-        </Text>
-      </View>
-    </ImageBackground>
+          {/* name and handle */}
+          <View>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 22,
+                fontWeight: "500",
+                marginBottom: 5,
+              }}
+            >
+              {user.name}
+            </Text>
+            <Text
+              style={{
+                color: "white",
+              }}
+            >
+              @{user.handle}
+            </Text>
+          </View>
+        </ImageBackground>
+      </Pressable>
+    </Link>
   );
 }
 
